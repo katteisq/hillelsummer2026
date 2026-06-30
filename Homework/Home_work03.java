a75161810f863be447b1cdcb225c24a91457f61e
Kathrin <ekaterinastareva1@gmail.com>
2026-06-30 20:39:32 +0200

    homework

--- Homework/Home_work03.java
+++ Homework/Home_work03.java
@@ -2,9 +2,6 @@ import java.util.Arrays;
 
 public class Home_work03 {
     static void main(String[] args) {
-        System.out.println("This is shop-online app");
-    }
-
-
+        System.out.println("Shop-online for customers");
     }
 }
