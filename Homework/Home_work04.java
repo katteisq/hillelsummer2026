939b9219eaef7224c214d4819b996de07f4bb0c6
Kathrin <ekaterinastareva1@gmail.com>
2026-07-07 23:01:04 +0200

    homework

--- Homework/Home_work04.java
+++ Homework/Home_work04.java
@@ -1,5 +1,30 @@
 public class Home_work04 {
     static void main(String[] args) {
-        double price = 123.4567458;
+        String product1 = "Smartphone";
+        String product2 = "Laptop";
+        double totalS = 12153.41d;
+        double totalL = 10486.85d;
+        int daysS = 5;
+        int daysL = 7;
+
+        double priceS = totalS / daysS;
+        double priceL = totalL / daysL;
+
+        System.out.println(" ----------------------------------------------------------------------");
+        System.out.printf("|  %-15s  |  EUR %-12s  |  %-10s  |  %-12s|%n", "Product", "Price", "Days", "Total");
+        System.out.println("|----------------------------------------------------------------------|");
+        System.out.printf("|  %-15s  |  EUR %-12f  |  %-10d  |  %-12.2f|%n", product1, priceS, daysS, totalS);
+        System.out.println("|----------------------------------------------------------------------|");
+        System.out.printf("|  %-15s  |  EUR %-12f  |  %-10d  |  %-12.2f|%n", product2, priceL, daysL, totalL);
+        System.out.println(" ----------------------------------------------------------------------");
     }
 }
+
+/* %12 - kolumna szerokością 12 symboli wyrównane do prawej
+    %-12 - -//- wurównana do lewej za pomocą minusa
+    s - string
+    d - decimal/integral (liczby całkowite)
+    f - floatinf point (ułamki)
+    %n - new line
+
+ */
