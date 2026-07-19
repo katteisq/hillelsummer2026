1cb1f0c7d48cda5cdaab17c0498de33c2f41a61c
Kathrin <ekaterinastareva1@gmail.com>
2026-07-19 13:11:33 +0200

    homework

---
+++ Homework/Home_work06.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Home_work06 {
+    static void main(String[] args) {
+    int sum = 0;
+
+        for (int i = 1; i <= 6; i++) {
+             sum += i;
+            System.out.println(i + ") " + "Num is " + i + ", sum is " + sum);
+        }
+        System.out.println("-----------------");
+        System.out.println("Sum of numbers is: " + sum);
+    }
+}
